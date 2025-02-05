3dd19483e54c7ab4005a496ea26092d6914a470f
MD MASUD RANA <86624203+masud51435@users.noreply.github.com>
2025-02-05 23:37:39 +0600

    otp page

--- lib/feature/authentication/otp/otp.dart
+++ lib/feature/authentication/otp/otp.dart
@@ -7,7 +7,7 @@ class OtpPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('OTP'),
+        title: const Text('OTP Page'),
       ),
     );
   }
